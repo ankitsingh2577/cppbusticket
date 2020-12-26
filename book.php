6f2f1964ecc22b3170d2a06eb3071c58350307ae
Ankit Singh <42251973+ankitsingh2577@users.noreply.github.com>
2020-12-26 11:55:06 +0000

    Update book.php

--- book.php
+++ book.php
@@ -11,7 +11,7 @@ $mobile=$_COOKIE["mobile"];
 //retrive name & email from cookies
 if($_COOKIE['email']==""){
 
-  header("Location: login.php");
+  header("Location: index.php");
 }
 //If cookies is present then page will load
 else{
@@ -46,7 +46,7 @@ if (!empty($_POST)) {
     if($result){
         echo '<script type="text/javascript">'; 
         echo 'alert("Ticket Booked Sucessfully");'; 
-        echo 'window.location.href = "index.php";';
+        echo 'window.location.href = "book.php";';
         echo '</script>';
         
         }
@@ -73,8 +73,8 @@ if (!empty($_POST)) {
     <div id="bg">
         
     </div>
-     <div><a class="logout" href="login.php" onclick="logout_cookie()">Logout</a></div>
-<div><a class="home" style="text-decoration:none" href="index.php">Home</a></div>
+     <div><a class="logout" href="index.php" onclick="logout_cookie()">Logout</a></div>
+<div><a class="home" style="text-decoration:none" href="home.php">Home</a></div>
 <div><a class="book" style="text-decoration:none" href="book.php">Book Ticket</a></div>
 <div><a class="ticket" style="text-decoration:none" href="myticket.php">My Ticket</a></div>
 <div><a class="my-account" style="text-decoration:none" href="account.php">Account</a></div>
