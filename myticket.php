88dd9690ee4c1006edc3a83452e28b786c417093
Ankit Singh <42251973+ankitsingh2577@users.noreply.github.com>
2020-12-26 12:45:06 +0000

    Update myticket.php

--- myticket.php
+++ myticket.php
@@ -36,8 +36,8 @@ else{ ?>
         
     </div>
  
- <div><a class="logout" href="login.php" onclick="logout_cookie()">Logout</a></div>   
-<div><a class="home" style="text-decoration:none" href="index.php">Home</a></div>
+ <div><a class="logout" href="index.php" onclick="logout_cookie()">Logout</a></div>   
+<div><a class="home" style="text-decoration:none" href="home.php">Home</a></div>
 <div><a class="book" style="text-decoration:none" href="book.php">Book Ticket</a></div>
 <div><a class="ticket" style="text-decoration:none" href="myticket.php">My Ticket</a></div>
 <div><a class="my-account" style="text-decoration:none" href="account.php">Account</a></div>
